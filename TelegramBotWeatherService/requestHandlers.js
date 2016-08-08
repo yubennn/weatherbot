be9808362aef20b032165b017821b4c6e9fe5724
yubennn <ben780614@gmail.com>
2016-08-08 16:06:45 +0800

    cert

--- TelegramBotWeatherService/requestHandlers.js
+++ TelegramBotWeatherService/requestHandlers.js
@@ -3,7 +3,7 @@ var fs = require("fs");
 var formidable = require("formidable");
 var connection = require("./dbConnection");
 var HongKongWeather = require('hongkong-weather');
-var hkweather = require("./hkＷeather");
+var hkweather = require("./hkWeather");
 var weatherRssHandler = require("./weatherRssHandler");
 var telegramBotUtil = require("./telegramBotUtil");
 
