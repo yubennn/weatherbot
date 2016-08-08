0e56ecded8d6a0f901c2ba36ce05f087859e30fc
yubennn <ben780614@gmail.com>
2016-08-08 20:51:54 +0800

    cert

--- TelegramBotWeatherService/index.js
+++ TelegramBotWeatherService/index.js
@@ -11,49 +11,51 @@ var app = express();
 var httpsServer = https.createServer(credentials, app);
 // server = https.createServer(credentials, app).listen(8443);
 httpsServer.listen(process.env.PORT || 5000,function(){
-  hkweather.updateRss('http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml', weatherRssHandler.checkRss);
-  timeout();
+  // hkweather.updateRss('http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml', weatherRssHandler.checkRss);
+  // timeout();
 });
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-//requesthandlers
-var requestHandlers = require("./requestHandlers");
-var handle = {}
-handle["/start"] = requestHandlers.start;
-handle["/subscribewarning"] = requestHandlers.subscribewarning;
-handle["/unsubscribewarning"] = requestHandlers.unsubscribewarning;
-handle["/tellmecurrent"] = requestHandlers.tellmecurrent;
-handle["/tellmewarning"] = requestHandlers.tellmewarning;
-//hkweather rss
-var hkweather = require("./hkWeather");
-var weatherRssHandler = require("./weatherRssHandler");
-function timeout(){
-  setTimeout(function () {
-    // hkweather.updateRss('http://rss.weather.gov.hk/rss/CurrentWeather_uc.xml', weatherRssHandler.checkRss);
-    hkweather.updateRss('http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml', weatherRssHandler.checkRss);
-    timeout();
-  },1000*30);
-}
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+//
+// //requesthandlers
+// var requestHandlers = require("./requestHandlers");
+// var handle = {}
+// handle["/start"] = requestHandlers.start;
+// handle["/subscribewarning"] = requestHandlers.subscribewarning;
+// handle["/unsubscribewarning"] = requestHandlers.unsubscribewarning;
+// handle["/tellmecurrent"] = requestHandlers.tellmecurrent;
+// handle["/tellmewarning"] = requestHandlers.tellmewarning;
+// //hkweather rss
+// var hkweather = require("./hkWeather");
+// var weatherRssHandler = require("./weatherRssHandler");
+// function timeout(){
+//   setTimeout(function () {
+//     // hkweather.updateRss('http://rss.weather.gov.hk/rss/CurrentWeather_uc.xml', weatherRssHandler.checkRss);
+//     hkweather.updateRss('http://rss.weather.gov.hk/rss/WeatherWarningBulletin_uc.xml', weatherRssHandler.checkRss);
+//     timeout();
+//   },1000*30);
+// }
 //get router
 app.post('/', function(req, res){
-  console.log(123);
-  'use strict';
-  //check method field exist
-  if(!req.body.message || !req.body.message.text){
-    res.send("There is not data coming!!");
-  }else{
-    var method = req.body.message.text;
-    if (typeof handle[method] === 'function') {
-      console.log("Method: " + method);
-      handle[method](res, req);
-    } else {
-      console.log("No request handler found for " + method);
-      res.writeHead(404, {"Content-Type": "text/html"});
-      res.write("404 Not found");
-      res.end();
-    }
-  }
+  // console.log(123);
+  // 'use strict';
+  // //check method field exist
+  // if(!req.body.message || !req.body.message.text){
+  //   res.send("There is not data coming!!");
+  // }else{
+  //   var method = req.body.message.text;
+  //   if (typeof handle[method] === 'function') {
+  //     console.log("Method: " + method);
+  //     handle[method](res, req);
+  //   } else {
+  //     console.log("No request handler found for " + method);
+  //     res.writeHead(404, {"Content-Type": "text/html"});
+  //     res.write("404 Not found");
+  //     res.end();
+  //   }
+  // }
+
+    res.send('123123123');
 });
 app.get('/', function(req, res){
   res.send('123123123');
